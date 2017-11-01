152e38248faef00f51572a5637ab7961b472af90
Filip Krikava <krikava@gmail.com>
2017-11-01 20:52:48 +0100

    Updated makefile to work on OSX

--- config.mk
+++ config.mk
@@ -7,18 +7,26 @@ VERSION = 0.8.1
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
-
 PKG_CONFIG = pkg-config
 
 # includes and libs
 INCS = -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
        `$(PKG_CONFIG) --cflags freetype2`
-LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
+LIBS = -L$(X11LIB)  -lm -lX11 -lutil -lXft \
        `$(PKG_CONFIG) --libs fontconfig` \
        `$(PKG_CONFIG) --libs freetype2`
+
+# X11 libs
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+    INCS += -I/usr/X11R6/include
+	LIBS += -I/usr/X11R6/lib -lrt
+endif
+ifeq ($(UNAME_S),Darwin)
+	INCS += -I/opt/X11/include
+    LIBS += -L/opt/X11/lib
+endif
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600

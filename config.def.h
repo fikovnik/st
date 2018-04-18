c4feb554370952a88faf80f74261e8aa306a4857
Filip Krikava <krikava@gmail.com>
2018-04-18 13:45:52 +0200

    Updated font to raw DejaVu

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+char *font = "DejaVu Sans Mono for Powerline:style=Book:pixelsize=13:antialias=true:autohint=true";
+int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:

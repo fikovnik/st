81d5ce373d5da32f9a3e1f9af6864e12fd7f525b
Filip Krikava <krikava@gmail.com>
2021-12-20 20:45:47 +0100

    Fix nord colors

--- my-colors.h
+++ my-colors.h
@@ -12,26 +12,29 @@
 
 
 static const char *colorname[] = {
-  "#2e3440", /* 0.  black   base00 */
-  "#bf616a", /* 1.  red     base0B */
-  "#a3be8c", /* 2.  green   base0E */
-  "#ebcb8b", /* 3.  yellow  base0D */
-  "#5e81ac", /* 4.  blue    base0A */
-  "#d08770", /* 5.  magenta base0C */
-  "#88c0d0", /* 6.  cyan    base08 */
-  "#e5e9f0", /* 7.  white   base05 */
-  "#4c566a", /* base03 */
-  "#81a1c1", /* base09 */
-  "#3b4252", /* base01 */
-  "#434c5e", /* base02 */
-  "#d8dee9", /* base04 */
-  "#eceff4", /* base06 */
-  "#b48ead", /* base0F */
-  "#8fbcbb", /* base07 */
+	// 8 normal colors
+  "#3b4252", /* 0. black   base00 */
+  "#bf616a", /* 1. red     base0B */
+  "#a3be8c", /* 2. green   base0E */
+  "#ebcb8b", /* 3. yellow  base0D */
+  "#81a1c1", /* 4. blue    base0A */
+  "#b48ead", /* 5. magenta base0C */
+  "#88c0d0", /* 6. cyan    base08 */
+  "#e5e9f0", /* 7. white   base05 */
+
+	// 8 bright colors
+  "#4c566a", /* 0. black   base03 */
+  "#bf616a", /* 1. red     base09 */
+  "#a3be8c", /* 2. green   base01 */
+  "#ebcb8b", /* 3. yellow  base02 */
+  "#81a1c1", /* 4. blue    base04 */
+  "#b48ead", /* 5. magenta base06 */
+  "#8fbcbb", /* 6. cyan    base0F */
+  "#eceff4", /* 7. white   base07 */
 
   [255] = 0,
-  "#cccccc",
-  "#555555",
+  "#2e3440",
+  "#d8dee9",
 };
 
 /*
@@ -39,7 +42,7 @@ static const char *colorname[] = {
 * foreground, background, cursor, reverse
 * cursor
 */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
